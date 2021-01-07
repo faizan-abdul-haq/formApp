9537a14f1c065b71af697ff45cbc18060d823983
faizan-abdul-haq <faizanabdulhaq@gmail.com>
2021-01-07 17:43:53 +0500

    Vue

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <toolbar></toolbar>
+  <app-footer></app-footer>
+</div>
+</template>
+
+<script>
+import AppFooter from './AppFooter'
+import toolbar from './Toolbar'
+export default {
+  components: { toolbar,AppFooter }
+
+}
+</script>
+
+<style>
+
+</style>
+
+
